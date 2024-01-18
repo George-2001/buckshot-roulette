711b8835eec4443f276459699e183e2901fd0d39
gchyoghly <gchyoghly@gmail.com>
2024-01-18 10:09:37 -0500

    Implemented shotgun load, rack and checkEmpty methods

--- src/main/java/com/georgechy/game/buckshotroulette/Shotgun.java
+++ src/main/java/com/georgechy/game/buckshotroulette/Shotgun.java
@@ -1,15 +1,58 @@
 package com.georgechy.game.buckshotroulette;
+import java.util.Stack;
 
-public interface Shotgun {
-    public void shootSelf();
-    public void shootOther();
-    public void load();
-    public void rack();
-    public void checkempty();
-    public void getCurrentShell();
-    public void getLastShell();
-    public void getShellsRemaining();
-    public int getNumBlankShells();
-    public int getNumLiveShells();
+public class Shotgun {
+    private Stack<Shell> shellStack;
+    ShellRandomizer shellRandomizer = new ShellRandomizer();
+    Shell chamberedShell;
+
+    /* Contructor */
+    public Shotgun(){
+        
+    }
+
+    public void shootPlayer(){
+
+    }
+    public void shootDealer(){
+
+    }
+    public void load(){
+        shellStack = shellRandomizer.getShells();
+    }
+    
+    public void rack(){
+        chamberedShell = shellStack.pop();
+        if (chamberedShell.isLive) {
+            System.out.println("Live Shell Ejected");
+        } else {
+            System.out.println("Blank Shell Ejected");
+        }
+    }
+
+    public boolean checkempty(){
+        if (shellStack.isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+    public void getCurrentShell(){
+
+    }
+    public void getLastShell(){
+
+    }
+    public void getShellsRemaining(){
+
+    }
+    public int getNumBlankShells(){
+
+    }
+    public int getNumLiveShells(){
+
+    }
 
 }
